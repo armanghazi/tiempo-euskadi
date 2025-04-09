3540608699741fa9b815d0a0f260c595fa13fa1e
Arman Ghaziaskari <103433612+ghaziaskari@users.noreply.github.com>
2025-04-09 13:26:46 +0200

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,12 +11,16 @@ const About = () => {
         <p>
           Esta aplicación muestra el tiempo meteorológico de las principales ciudades de Euskadi.
           Puedes seleccionar una provincia para ver el tiempo de sus ciudades y añadir tus favoritas.
+          Con la funcionalidad de la barra de navegación, puedes acceder a la sección de favoritos y a la sección de acerca de.
+          Puedes cambiar el tema de la aplicación utilizando el botón de cambio de tema.
+
         </p>
         <p>
-          Los datos meteorológicos son proporcionados por Open-Meteo.
+          Los datos meteorológicos son obtenidos de la API de Open-Meteo.
+          Los datos son gratis y se actualizan en tiempo real.
         </p>
 
-        <h2>Recursos utilizados</h2>
+        <h2>Recurso utilizado</h2>
         <ul>
           <li><a href="https://open-meteo.com/" target="_blank" rel="noopener noreferrer">Open-Meteo</a></li>
         </ul> 
